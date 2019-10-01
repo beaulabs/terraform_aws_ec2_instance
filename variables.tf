9e95a02a099ba752afc379d5b38523fb2c886445
Beau Labs <46016124+beaulabs@users.noreply.github.com>
2019-10-01 13:26:16 -0600

    change size

--- variables.tf
+++ variables.tf
@@ -26,7 +26,7 @@ variable "ami_id" {
 
 variable "instance_type" {
   description = "type of EC2 instance to provision."
-  default     = "t2.micro"
+  default     = "m4.large"
 }
 
 variable "myaws_keypair" {
